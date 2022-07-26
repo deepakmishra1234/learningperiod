c3516bb2332b0b944cfd24067ecdce9a5eaf68a1
deepakmishra1234 <109963531+deepakmishra1234@users.noreply.github.com>
2022-07-26 12:23:21 +0530

    Add files via upload

---
+++ DataTypes1.java
@@ -0,0 +1,27 @@
+package phase1.mphasis;
+
+public class DataTypes1 {
+
+	public static void main(String[] args)
+	{
+	  int a= 78;
+	  long l = 3467;
+	  float f = 87.9f;
+	  double d = 98.9999;
+	  char ch = 's';
+	 
+	 System.out.println("This is my program");
+	 System.out.println(a);
+	 System.out.println(l);
+	 System.out.println(f);
+	
+	 System.out.println(d);
+	
+	    
+	    
+	  
+	  
+
+	}
+
+}
