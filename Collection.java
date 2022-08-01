80326067433c80b607d53db7048d664a366a1380
deepakmishra1234 <109963531+deepakmishra1234@users.noreply.github.com>
2022-08-01 11:49:16 +0530

    Add files via upload

---
+++ Collection.java
@@ -0,0 +1,22 @@
+package practiceproject.phase1;
+
+import java.util.*;
+  
+public class Collection 
+{
+
+	public static void main(String[] args)
+	{
+        ArrayList<String> al=new ArrayList<String>();  
+		
+		System.out.println("Initial size of Arraylist is "+ al.size());  
+		
+		al.add("Hello");
+		al.add("World");
+		System.out.println("Arraylist is "+al);
+		System.out.println("New size of Arraylist is "+al.size());
+		
+
+	}
+
+}
