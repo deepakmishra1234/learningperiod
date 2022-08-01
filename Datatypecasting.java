55a0c6b936a0371bc8da54282c6f2feb3274d7af
deepakmishra1234 <109963531+deepakmishra1234@users.noreply.github.com>
2022-08-01 11:47:43 +0530

    Add files via upload

---
+++ Datatypecasting.java
@@ -0,0 +1,27 @@
+package practiceproject.phase1;
+
+public class Datatypecasting {
+
+	public static void main(String[] args)
+	{
+		float f =  34.7f;
+		long k = (long)f;
+		System.out.println("value of float is:" +f);
+		System.out.println("value of k  is:" +k);
+		
+		double d= 8688.99;
+		long l2= (long)d;
+		System.out.println("value of double is:" +d);
+		System.out.println("value of double to long is:" +l2);
+		 int a = (int)l2;
+		 System.out.println("value of long to int is:" +a);
+		 char ch=(char)a;
+		 System.out.println("value of int to character is:" +ch);
+		 byte b = (byte)(l2);
+		 System.out.println("value of long to byte is:" +b);
+		 
+		 
+		
+	}
+
+}
