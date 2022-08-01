507aeddf391864c7be2a565adc1eb8ae07a1ab40
deepakmishra1234 <109963531+deepakmishra1234@users.noreply.github.com>
2022-08-01 11:54:22 +0530

    Add files via upload

---
+++ Trycatch.java
@@ -0,0 +1,19 @@
+package practiceproject.phase1;
+
+public class Trycatch
+{
+	public static void main(String[ ] args) 
+	{
+	    try {
+	    	
+	      int[] myNumbers = {1, 2, 3};
+	      System.out.println(myNumbers[10]);
+	    } 
+	    
+	    catch (Exception e) 
+	    {
+	      System.out.println("Something went wrong.");
+	    }
+ 
+	}
+}
