eb92830ac23cd20e60b3e71001dd9bfec1297c9c
deepakmishra1234 <109963531+deepakmishra1234@users.noreply.github.com>
2022-08-03 13:43:56 +0530

    Add files via upload

---
+++ Arrayrotation.java
@@ -0,0 +1,32 @@
+package datastructure.phase1;
+
+import java.io.*;
+	public class Arrayrotation
+	{
+		public static void rotate(int arr[], int d, int n)
+		{
+			int p = 1;
+			while (p <= d) {
+				int last = arr[0];
+				for (int i = 0; i < n - 1; i++) {
+					arr[i] = arr[i + 1];
+				}
+				arr[n - 1] = last;
+				p++;
+			}
+
+			for (int i = 0; i < n; i++) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+		
+		public static void main(String[] args)
+		{
+			int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+			int N = arr.length;
+			// Rotate 5 times
+			int d = 5;
+			rotate(arr, d, N);
+		}
+	}
+	
